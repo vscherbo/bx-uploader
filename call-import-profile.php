fb18eddb575ead7150b143afe007a62e617bc6de
Vladimir Scherbo <vscherbo@gmail.com>
2015-07-10 12:43:38 +0300

    создан скрипт call-import-profile.php

---
+++ call-import-profile.php
@@ -0,0 +1,29 @@
+#!/usr/bin/env php
+<?php
+require("set-doc-root.php");
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+
+CModule::IncludeModule("iblock");
+
+        AddEventHandler("iblock", "OnAfterIBlockSectionAdd", "OnAfterAddImport");
+        AddEventHandler("iblock", "OnAfterIBlockSectionUpdate", "OnAfterAddImport");
+        function OnAfterAddImport($arElement)
+        {
+        // print "AfterAdd ". $arElement["IBLOCK_ID"] . "\n";
+                if (in_array($arElement["IBLOCK_ID"], Array(30)))
+                {
+                        $arOriginalElement = $arElement;
+
+                        $arElement["XML_ID"] = $arElement["ID"];
+
+                        if ($arOriginalElement !== $arElement)
+                        {
+                                $obSection = new CIBlockSection;
+                                $obSection->Update($arElement["ID"], $arElement);
+                        }
+                        return true;
+                }
+        }
+
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/php_interface/include/catalog_import/cron_frame.php");
+?>
