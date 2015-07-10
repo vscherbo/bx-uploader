85daa9b76c8913c839d1639e2b7827e3f3fd1a46
Vladimir Scherbo <vscherbo@gmail.com>
2015-07-10 12:42:54 +0300

    вызов call-import-profile.php с установкой обработчиков для секций ib30 для заполнения XML_ID

--- run-import-profile.sh
+++ run-import-profile.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 #time 
 [ +$1 == + ] && { echo "1st parameter import_profile_ID is missed."; exit 123; }
-/usr/bin/php -f /home/bitrix/www/bitrix/php_interface/include/catalog_import/cron_frame.php $1
+
+# /usr/bin/php -f /home/bitrix/www/bitrix/php_interface/include/catalog_import/cron_frame.php $1
+
+/usr/bin/php $HOME/call-import-profile.php $1
 
