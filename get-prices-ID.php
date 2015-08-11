4b019529f03bacbe65047d4e9b8770c52dfe61e2
Vladimir Scherbo <vscherbo@gmail.com>
2015-08-11 10:36:08 +0300

    полностью переписан: поиск по ib29 (каталог) property_674 вместо поиска по имени в ib30 (модификации)

--- get-prices-ID.php
+++ get-prices-ID.php
@@ -1,30 +1,34 @@
 #!/usr/bin/env php
 <?php
-// $_SERVER["DOCUMENT_ROOT"] = "/home/bitrix/ext_www/kipspb2.arc.world";
 require("set-doc-root.php");
 
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+CModule::IncludeModule("catalog");
 
 if ($argv[1] == "")
 {
-    fwrite(STDERR, $argv[0]." ERROR: 1st parameter Model_name is required.\n" );
+    fwrite(STDERR, $argv[0]." ERROR: 1st parameter Model_name is required.\n");
     exit(1);
 }
 
 $arFilter = array(
-    "IBLOCK_ID" => "30",
+    "IBLOCK_ID" => "29",
     "NAME" => $argv[1],
     "ACTIVE" => "Y",
-    //"NAME" => "AR-HP350",
 );
 
-$arSelect = Array("ID", "NAME", "DATE_ACTIVE_FROM");
-$rsItems = CIBlockSection::GetList(Array("SORT" => "ASC"), $arFilter, false, false, $arSelect);
+//674 - модификации
+//675 - модификаторы
+$arSelect = Array("ID", "NAME", "TIMESTAMP_X", "MODIFIED_BY", "PROPERTY_674", "PROPERTY_675" );
+
+$el = new CIBlockElement;
+$rsItems = $el->GetList(Array("SORT" => "ASC"), $arFilter, false, false, $arSelect);
 
 while($ob = $rsItems->GetNextElement())
 {
- $arFields = $ob->GetFields();
- echo $arFields["ID"];
+    $arFields = $ob->GetFields();
+    //$devName = $arFields["NAME"];
+    echo $arFields["PROPERTY_674_VALUE"];
 }
 
 ?>
