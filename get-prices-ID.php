7dd86e5dc391d0ead1bbc4d17f4c3a6b360b6bc3
Vladimir Scherbo <vscherbo@gmail.com>
2015-08-12 12:20:10 +0300

    работающая версия со сравнением модификаций из свойства 674 и по имени

--- get-prices-ID.php
+++ get-prices-ID.php
@@ -3,7 +3,6 @@
 require("set-doc-root.php");
 
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
-CModule::IncludeModule("catalog");
 
 if ($argv[1] == "")
 {
@@ -12,17 +11,13 @@ if ($argv[1] == "")
 }
 
 $arFilter = array(
-    "IBLOCK_ID" => "29",
+    "IBLOCK_ID" => "30",
     "NAME" => $argv[1],
     "ACTIVE" => "Y",
 );
 
-//674 - модификации
-//675 - модификаторы
-$arSelect = Array("ID", "NAME", "TIMESTAMP_X", "MODIFIED_BY", "PROPERTY_674", "PROPERTY_675" );
-
-$el = new CIBlockElement;
-$rsItems = $el->GetList(Array("SORT" => "ASC"), $arFilter, false, false, $arSelect);
+$arSelect = Array("ID", "NAME", "DATE_ACTIVE_FROM");
+$rsItems = CIBlockSection::GetList(Array("SORT" => "ASC"), $arFilter, false, false, $arSelect);
 
 while($ob = $rsItems->GetNextElement())
 {
