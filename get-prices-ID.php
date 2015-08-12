5a69028ee567de4f7bf5677a9e1a113f48658d04
Vladimir Scherbo <vscherbo@gmail.com>
2015-08-12 11:41:41 +0300

    поиск и сравнение модификаций по свойству 674 и по имени, возврат ошибки при несовпадении

--- get-prices-ID.php
+++ get-prices-ID.php
@@ -15,7 +15,6 @@ $arFilter = array(
     "IBLOCK_ID" => "30",
     "NAME" => $argv[1],
     "ACTIVE" => "Y",
-    //"NAME" => "AR-HP350",
 );
 
 $arSelect = Array("ID", "NAME", "DATE_ACTIVE_FROM");
@@ -24,7 +23,59 @@ $rsItems = CIBlockSection::GetList(Array("SORT" => "ASC"), $arFilter, false, fal
 while($ob = $rsItems->GetNextElement())
 {
  $arFields = $ob->GetFields();
- echo $arFields["ID"];
+ $ib30_id = $arFields["ID"];
+ // echo $arFields["ID"];
 }
+
+//////////////////////////////////////////////////////////////////////////////////
+
+$arFilter = array(
+    "IBLOCK_ID" => "29",
+    "NAME" => $argv[1],
+    "ACTIVE" => "Y",
+);
+
+//674 - модификации
+//675 - модификаторы
+$arSelect = Array("ID", "NAME", "TIMESTAMP_X", "MODIFIED_BY", "PROPERTY_674", "PROPERTY_675" );
+
+$el = new CIBlockElement;
+$rsItems = $el->GetList(Array("SORT" => "ASC"), $arFilter, false, false, $arSelect);
+
+while($ob = $rsItems->GetNextElement())
+{
+    $arFields = $ob->GetFields();
+    //$devName = $arFields["NAME"];
+    $ib29_prop674 = $arFields["PROPERTY_674_VALUE"];
+}
+
+if ($ib30_id != $ib29_prop674 ) {
+	$arFilter = array(
+	    "IBLOCK_ID" => "30",
+	    "XML_ID" => $ib29_prop674,
+	    "ACTIVE" => "Y",
+	);
+
+	$arSelect = Array("ID", "NAME", "DATE_ACTIVE_FROM");
+	$rsItems = CIBlockSection::GetList(Array("SORT" => "ASC"), $arFilter, false, false, $arSelect);
+
+	while($ob = $rsItems->GetNextElement())
+	{
+	 $arFields = $ob->GetFields();
+	 $prop674_name = $arFields["NAME"];
+	}
+        fwrite(STDERR, "Model_name=". $argv[1] . ", PROP674=". $ib29_prop674 . " with Name=". $prop674_name . ", Modifications_byName_ID=". $ib30_id ."\n");
+} else {
+        echo $ib30_id;
+}
+
+
+
+
+
+
+
+
+
 
 ?>
