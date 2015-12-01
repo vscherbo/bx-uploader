c2d697e7b657419c2569052e4cf2536c6f08e60b
Vladimir Scherbo <vscherbo@gmail.com>
2015-12-01 16:43:26 +0300

    убрана закомментированная строка с абсолютным путём

--- props.php
+++ props.php
@@ -1,6 +1,5 @@
 <?
 
-// $_SERVER["DOCUMENT_ROOT"] = "/home/bitrix/ext_www/kipspb2.arc.world";
 require("set-doc-root.php");
 
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
