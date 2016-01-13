171ec844bfb79b5f1f2bb567e387937536c4dc75
Vladimir Scherbo <vscherbo@gmail.com>
2016-01-13 13:47:20 +0300

    исправлен путь для kippsb-fl, добавлены переменные

--- set-doc-root.php
+++ set-doc-root.php
@@ -1,4 +1,6 @@
 <?
-$_SERVER["DOCUMENT_ROOT"] = "/home/bitrix/ext_www/kipspb2.arc.world";
+$_SERVER["DOCUMENT_ROOT"] = "/home/bitrix/ext_www/kipspb-fl.arc.world";
 $DOCUMENT_ROOT = $_SERVER["DOCUMENT_ROOT"] ;
+define("NO_KEEP_STATISTIC", true);
+define("NOT_CHECK_PERMISSIONS", true);
 ?>
