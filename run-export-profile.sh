4c315e008ef6a79545fc387c7972aacd87f28328
Vladimir Scherbo <vscherbo@gmail.com>
2016-02-09 09:33:25 +0300

    добавлен ionice и nice

--- run-export-profile.sh
+++ run-export-profile.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #time 
 [ +$1 == + ] && { echo "1st parameter export_profile_ID is missed."; exit 123; }
-/usr/bin/php -f /home/bitrix/www/bitrix/php_interface/include/catalog_export/cron_frame.php $1
+/bin/nice -n19 /usr/bin/ionice -c2 -n7 /usr/bin/php /home/bitrix/www/bitrix/php_interface/include/catalog_export/cron_frame.php $1
+
 
