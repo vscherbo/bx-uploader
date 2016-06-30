5d710af2eadc02b35eca1793e273d2a711658542
Vladimir Scherbo <vscherbo@gmail.com>
2016-06-30 16:01:32 +0300

    контроль одноимённый модификаторов с сообщением

--- get-modificators-ID.php
+++ get-modificators-ID.php
@@ -21,11 +21,20 @@ $arFilter = array(
 $arSelect = Array("ID", "NAME", "DATE_ACTIVE_FROM");
 $rsItems = CIBlockElement::GetList(Array("SORT" => "ASC"), $arFilter, false, false, $arSelect);
 
+$cnt = 0;
+$mod_id = 0;
 while($ob = $rsItems->GetNextElement())
 {
- $arFields = $ob->GetFields();
- //print_r($arFields);
- print_r($arFields["ID"]);
+    $arFields = $ob->GetFields();
+    $cnt++;
+    //print_r($arFields["ID"]);
+    $mod_id = $arFields["ID"];
+}
+
+if ( $cnt > 1 ) {
+    fwrite(STDERR, $argv[0]." ERROR: Найдено ".$cnt." описания(-й) модификаторов с именем ".$argv[1] .".\n" );
+} else {
+    echo $mod_id;
 }
 
 ?>
