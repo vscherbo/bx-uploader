9e3f553eb7e637f64a486607798693adfc0dfcc9
Vladimir Scherbo <vscherbo@gmail.com>
2017-03-29 16:05:45 +0300

    fwrite to STDERR

--- order-status-safe-update.php
+++ order-status-safe-update.php
@@ -1,21 +1,18 @@
 #!/usr/bin/env php
 <?php
-// $_SERVER["DOCUMENT_ROOT"] = "/home/bitrix/ext_www/kipspb2.arc.world";
 require("set-doc-root.php");
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
 CModule::IncludeModule("sale");
 
-$STDERR = fopen('php://stderr', 'w+');
-
 if ($argv[1] == "")
 {
-    fwrite($STDERR, $argv[0]." ERROR: 1st parameter Order_ID is required.");
+    fwrite(STDERR, $argv[0]." ERROR: 1st parameter Order_ID is required.");
     exit(1);
 }
 
 if ($argv[2] == "")
 {
-    fwrite($STDERR, $argv[0]." ERROR: 2nd parameter New_status is required.");
+    fwrite(STDERR, $argv[0]." ERROR: 2nd parameter New_status is required.");
     exit(2);
 }
 
@@ -25,7 +22,7 @@ $NEW_STATUS = $argv[2];
 // Get current status
 if (!($arOrder = CSaleOrder::GetByID($ORDER_ID )))
 {
-   fwrite($STDERR, "Заказ с кодом ".$ORDER_ID." не найден");
+   fwrite(STDERR, "Заказ с кодом ".$ORDER_ID." не найден");
    exit(3);
 }
 else
@@ -45,19 +42,18 @@ while($ob = $rsItems->Fetch())
 if ( $orderStatus[$NEW_STATUS] > $orderStatus[$currentStatus] ) {
     $USER = new CUser;
     if (!$USER->Authorize(6575)) {
-        fwrite($STDERR, "order-status-safe-update: Ошибка авторизации") ;
+        fwrite(STDERR, "order-status-safe-update: Ошибка авторизации") ;
         exit(4);
     }
 
 	// Update
 	if (!CSaleOrder::StatusOrder($ORDER_ID, $NEW_STATUS)) {
-           fwrite($STDERR, "Ошибка установки нового статуса заказа");
+           fwrite(STDERR, "Ошибка установки нового статуса заказа");
            exit(5);
         }
 } elseif ( $orderStatus[$NEW_STATUS] < $orderStatus[$currentStatus] ) {
-        fwrite($STDERR, "Текщий статус заказа " .$currentStatus. " больше, чем новый ".$NEW_STATUS. "") ;
+        fwrite(STDERR, "Текщий статус заказа " .$currentStatus. " больше, чем новый ".$NEW_STATUS. "") ;
         exit(6);
 }
 
-fclose($STDERR);
 ?>
